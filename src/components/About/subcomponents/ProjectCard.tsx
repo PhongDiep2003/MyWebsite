5517ac1ad78849d732a08673fa0a9489bb3d28fa
PhongDiep <phongdiep0712@gmail.com>
2023-12-28 21:49:43 -0800

    Add chatgpt clone project to the project section

--- src/components/About/subcomponents/ProjectCard.tsx
+++ src/components/About/subcomponents/ProjectCard.tsx
@@ -4,9 +4,10 @@ interface ProjectCardProps {
   name: string;
   url: string;
   description: string;
-  githubLink: string;
+  githubLink?: string;
+  projectUrl?: string;
 }
-const ProjectCard = ({name, url, description, githubLink}: ProjectCardProps) => {
+const ProjectCard = ({name, url, description, githubLink, projectUrl}: ProjectCardProps) => {
   const [animate, setAnimate] = useState(false) //prevent animation is applied on first render
   const [onFront, setOnFront] = useState(true);
   const [frontToBack, setFrontToBack] = useState(true)
@@ -25,7 +26,8 @@ const ProjectCard = ({name, url, description, githubLink}: ProjectCardProps) => 
                 <div className="back__cover">
                   <h4 className="card__title">Name: <span className='card__content'>{name}</span> </h4>
                   <h4 className="card__title">Description: <span className='card__content' >{description}</span></h4>
-                  <h4 className='card__title'>Github Repo: <a className='card__content' href={githubLink} target='_blank'>{githubLink}</a></h4>
+                  {githubLink && <h4 className='card__title'>Github Repo: <a className='card__content' href={githubLink} target='_blank'>{githubLink}</a></h4>}
+                  {projectUrl && <h4 className='card__title'>Project URL: <a className='card__content' href={projectUrl} target='_blank'>{projectUrl}</a></h4>}
                 </div>
       }
     </div>
--- src/components/About/subcomponents/ProjectSection.tsx
+++ src/components/About/subcomponents/ProjectSection.tsx
@@ -3,6 +3,7 @@ import './css/project.css'
 import ProjectCard from './ProjectCard'
 import ScanSlateImg from '../../../images/ScanSlate.png'
 import GmailCloneImg from '../../../images/GmailCloneLogo.png'
+import ChatGPTCloneImg from '../../../images/chatgptImage.jpeg'
 const projects = [{
   name: 'ScanSlate',
   description: "A mobile app developed for the CMPE 133 group project. allows users to capture a picture of an object and label it using TensorFlow's MobileNet image classification, and then translate that label into one of the many languages that users selected when creating their account. The app includes a quiz mode feature-based flashcard meant to help users learn and reinforce their memory. Aside from that, the app also offers some basic flashcard management features such as removing and viewing flashcards.",
@@ -11,9 +12,15 @@ const projects = [{
 },
 {
   name: 'Gmail Clone',
-  description: "Cloned Google Gmail application that replicates all of Google Gmail's basic capabilities such as sign up, login, email composition, and so on. Furthermore, this cloned Google Gmail enables more additional functionalities such as creating todo lists and adding contacts, which enhances user experience and makes it more of a management and collaboration tool for users.",
+  description: "Cloned the Google Gmail application, which simulates every fundamental feature of Google Gmail, including registration, authentication, and email composition. Furthermore, this cloned version of Google Gmail provides extra functionalities, such as the ability to add contacts and create to-do lists. These additions are intended to improve the overall user experience and transform this cloned application into a more collaborative and managed tool for its users.",
   url: GmailCloneImg,
   githubLink: 'https://github.com/PhongDiep2003/CMPE131-Project'
+},
+{
+  name: 'ChatGPT Clone',
+  description: "Utilized OpenAI API to clone the ChatGPT application and incorporated an additional feature to the cloned application, namely speech-to-text feature. This speech-to-text feature enables users to input their questions by utilizing the built-in microphone on their computer, which transcribes their spoken words into text and sends them to the API. The application also includes a text input, which allows users, who choose not to use their microphone, to send question to the API by typing it.",
+  url: ChatGPTCloneImg,
+  projectUrl: 'https://chatgptclone-ea865.web.app/'
 }]
 const ProjectSection = () => {
   return (
